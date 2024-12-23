6dbdb3e09d7552bc0c535a7411d3ed6b652e8fd1
muaz64 <muazmuhammad21@gmail.com>
2024-12-23 20:29:24 +0600

    User.tsx (version-2) type method

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,10 +1,32 @@
-// User.tsx (version-1) normal way
-const User = (props: {
+// User.tsx (version-1) normal method
+// const User = (props: {
+//   name: string;
+//   email: string;
+//   age: number;
+//   isRegistered: boolean;
+// }) => {
+//   return (
+//     <div style={{ border: "1px solid", margin: "1rem" }}>
+//       <h2>{props.name}</h2>
+//       <p>{props.email}</p>
+//       <p>{props.age} years old</p>
+//       {props.isRegistered ? (
+//         <p>Registered Student</p>
+//       ) : (
+//         <p>Unregistered Student</p>
+//       )}
+//     </div>
+//   );
+// }; 
+
+// User.tsx (version-2) type method
+type UserProps = {
   name: string;
   email: string;
   age: number;
   isRegistered: boolean;
-}) => {
+};
+const User = (props: UserProps) => {
   return (
     <div style={{ border: "1px solid", margin: "1rem" }}>
       <h2>{props.name}</h2>
@@ -18,5 +40,6 @@ const User = (props: {
     </div>
   );
 };
+
 
 export default User
