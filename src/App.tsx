fbcb2a75e15de159516256cf80fd5df0785c3de0
muaz64 <muazmuhammad21@gmail.com>
2024-12-31 21:46:03 +0600

    card.tsx add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 
-import Button from './components/Button';
+
 import Post from './components/Post';
 
 // const user1 = {
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,10 +1,10 @@
-
+import Card from "./Card";
 const Post = () => {
   return (
-    <div>
+    <Card>
    <h3> Post Title</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, sed dolor. Cum dolor quisquam maiores harum dignissimos impedit ab voluptate.</p>
-  </div>
+  </Card>
   );
   
 }
